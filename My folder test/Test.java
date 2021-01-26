bb3bcfd7ccdf9f6f44c4245b69b98764263faa54
Joy <joyjk99@gmail.com>
2021-01-26 13:43:06 +0600

    This is modefied by Joy

---
+++ My folder test/Test.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Test {
+
+public static void main (String[] args) {
+	
+		Box box = new Box();
+		box.setLength(5.5);
+		box.setWidht(2.5);
+		box.setHeigth(2.0);
+		
+		System.out.println("box Length: "+box1.getLength());
+		System.out.println("box Wight: "+box1.getWidth());
+		System.out.println("box Heigth: "+box1.getHeigth());
+                System.out.println("Price: "+box1.getpriceofBox());
+		 
+		
+		
+
+	}
+
+}
